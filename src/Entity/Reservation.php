64f8f39e3512655579b1f4bcf5bad31faf9c35ee
Gionnah <vmgionah@gmail.com>
2025-07-16 15:59:30 +0300

    first commit

---
+++ src/Entity/Reservation.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ReservationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ReservationRepository::class)]
+class Reservation
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 80)]
+    private $nom_client;
+
+    #[ORM\Column(type: 'datetime')]
+    private $date_heure;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomClient(): ?string
+    {
+        return $this->nom_client;
+    }
+
+    public function setNomClient(string $nom_client): self
+    {
+        $this->nom_client = $nom_client;
+
+        return $this;
+    }
+
+    public function getDateHeure(): ?\DateTimeInterface
+    {
+        return $this->date_heure;
+    }
+
+    public function setDateHeure(\DateTimeInterface $date_heure): self
+    {
+        $this->date_heure = $date_heure;
+
+        return $this;
+    }
+}
